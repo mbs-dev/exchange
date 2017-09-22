8f2734438de16c906bbc5bfb4760d177581c33e3
Vladimir Ignatev <ya.na.pochte@gmail.com>
2017-09-23 01:44:55 +0400

    main updates

--- index.js
+++ index.js
@@ -1,25 +1,69 @@
+const config = require('./exchanges.config.js')
+
 const lo = require('lodash')
-const utils = require('./src/utils')
-
-const exchanges = {
-  'Bittrex': require('./src/exchanges/bittrex').getOrderBook,
-  'Poloniex': require('./src/exchanges/poloniex').getOrderBook,
-  'Bitfinex': require('./src/exchanges/bitfinex').getOrderBook
-}
-
+const utils = require('./data/utils')
+const EventEmitter = require('events')
 const market = utils.parseMarketFromCli()
+
+const ExchangeWatcher = require('./data/watcher/watcher.js')
+
+const db = require('./db.js')
+
+const dataEmitter = new EventEmitter()
 
 if (!market) {
   console.log('no valid market provided')
   process.exit(1)
 }
 
-Promise.all(lo.values(lo.each(exchanges, (getOrderBook, exchangeName) => {
-  const _utils = utils
-  return getOrderBook(market).then((ordersDict) => {
-    console.log('-- ' + exchangeName + ' --')
-    _utils.stupidOrderBookFormat(ordersDict['BUY'], ordersDict['SELL'])
-  }).catch((reason)=>{
-    console.error('[' + exchangeName + '] Unable to retrieve data: ' + reason);
+dataEmitter.on('exchangeData', (data) => {
+  // console.log('-- ' + data['exchange'] + ' --')
+  // utils.stupidOrderBookFormat(data['orderBook']['buy'], data['orderBook']['sell'])
+
+  const marketFirst = data['market'].first
+  const marketSecond = data['market'].second
+  const exchange = data['exchange']
+  const orderBook = data['orderBook']
+
+  const now = new Date()
+
+
+  //todo: remove duplicated code
+  const dataBatch = []
+
+  lo.each(orderBook['buy'], (order, index) => {
+    dataBatch.push({
+      exchange: exchange,
+      marketFirst: marketFirst,
+      marketSecond: marketSecond,
+      type: utils.consts.buy,
+      volume: order[utils.consts.volume],
+      price: order[utils.consts.price],
+      created_at: now,
+      updated_at: now
+    })
   })
-})))
+
+  //todo: remove duplicated code
+  lo.each(orderBook['sell'], (order, index) => {
+    dataBatch.push({
+      exchange: exchange,
+      marketFirst: marketFirst,
+      marketSecond: marketSecond,
+      type: utils.consts.sell,
+      volume: order[utils.consts.volume],
+      price: order[utils.consts.price],
+      created_at: now,
+      updated_at: now
+    })
+  })
+
+  db.batchInsert('orders', dataBatch, 30).then((res)=>{
+    console.log('Data from ' + data['exchange'] + ' retrieved and saved.')
+  }).catch((err)=>{
+    console.log(err);
+  })
+})
+
+const ew = new ExchangeWatcher(config, 15000, market, dataEmitter)
+ew.enable()
